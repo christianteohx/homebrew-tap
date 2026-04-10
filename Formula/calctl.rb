f9c11de50075a2b4d8b64bd31078777eee6d9a66
Christian Teo <christeo2001@hotmail.com>
2026-04-10 18:20:08 -0500

    Formula: fix Ruby syntax bug - hardcode SHA256

--- Formula/calctl.rb
+++ Formula/calctl.rb
@@ -2,7 +2,7 @@ class Calctl < Formula
   desc "Native macOS Apple Calendar CLI via EventKit"
   homepage "https://github.com/christianteohx/calctl"
   url "https://github.com/christianteohx/calctl/releases/download/v1.1.0/calctl"
-  sha256 "#{NEW_SHA}"
+  sha256 "209db587ffb3cc6ec81615cbd4dd5ea74067a0f4b6a0844731ff8c1a043d0731"
   license "MIT"
   version "1.1.0"
 
