879217f7f1df3e5581eaf2e5b5f2c907ffe5d2c6
Christian Teo <christeo2001@hotmail.com>
2026-04-10 17:56:52 -0500

    Formula: update calctl to v1.1.0

--- Formula/calctl.rb
+++ Formula/calctl.rb
@@ -1,10 +1,10 @@
 class Calctl < Formula
   desc "Native macOS Apple Calendar CLI via EventKit"
   homepage "https://github.com/christianteohx/calctl"
-  url "https://github.com/christianteohx/calctl/releases/download/v1.0.0/calctl"
-  sha256 "f1dc16290f353b3e6911350eff8f7026ddd365fe0e19c18b1c8b0721c2560868"
+  url "https://github.com/christianteohx/calctl/releases/download/v1.1.0/calctl"
+  sha256 "#{NEW_SHA}"
   license "MIT"
-  version "1.0.0"
+  version "1.1.0"
 
   depends_on :macos => :sonoma
 
