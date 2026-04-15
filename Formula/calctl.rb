772ec36c65cba24c3406bc13dbfc267e51451351
Christian Teo <christeo2001@hotmail.com>
2026-04-15 15:03:37 -0500

    calctl: update to v1.3.0 (--attendees feature)

--- Formula/calctl.rb
+++ Formula/calctl.rb
@@ -1,10 +1,10 @@
 class Calctl < Formula
   desc "Native macOS Apple Calendar CLI via EventKit"
   homepage "https://github.com/christianteohx/calctl"
-  url "https://github.com/christianteohx/calctl/releases/download/v1.2.0/calctl"
+  url "https://github.com/christianteohx/calctl/releases/download/v1.3.0/calctl"
   sha256 "15efe7a6ef826703e3d54a87704f54b3758f4f0e688a2d5f5b2b23837bc78a6d"
   license "MIT"
-  version "1.2.0"
+  version "1.3.0"
 
   depends_on :macos => :sonoma
 
